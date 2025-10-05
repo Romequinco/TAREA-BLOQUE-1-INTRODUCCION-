b4de9eff91644f9f095f9f85a18373c924a26092
Romequinco <romequinco@gmail.com>
2025-10-05 20:30:00 +0200

    Update codigo.py

--- src/codigo.py
+++ src/codigo.py
@@ -1,1 +1,9 @@
+import numpy as np
+import yahoofinance as yf
+import pandas as pd
+import datetime 
+import random
+#primero importamos las librerias necesarias para la ejecucion del programa
+
+
 
