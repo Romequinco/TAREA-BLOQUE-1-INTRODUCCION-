e2319b3115f18ab2e4ec0ba79321474704848fb0
Romequinco <romequinco@gmail.com>
2025-10-05 20:46:04 +0200

    Update codigo.py

--- src/codigo.py
+++ src/codigo.py
@@ -5,5 +5,13 @@ import datetime
 import random
 #primero importamos las librerias necesarias para la ejecucion del programa
 
+#seleccionamos las apis de las que sacaremos informaciones
+import api1
+import api2
+import api3
+datos_1 = api1
+datos_2 = api2{precio_historico)
+datos_3 = api3(precio_hisotrico)
 
-
+print("Que informacion deseas?\n
+      ")
