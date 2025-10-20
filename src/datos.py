48f0d18f5bdac2cb2d0730764f6900d14e16a9dd
Romen <romequinco@gmail.com>
2025-10-20 18:25:04 +0200

    Import requests and initialize URL variable
    Added requests import and initialized URL variable.

--- src/datos.py
+++ src/datos.py
@@ -2,8 +2,10 @@
 import numpy as np
 import yfinance as yf
 import pandas as pd
-
+import requests #para importar las APIs
 #recordar importar las funciones del utils.py
+
+url = "
 
 
 #seleccionamos las apis de las que sacaremos informaciones
@@ -16,4 +18,5 @@ tickers_list = yf.ticker
 
 datos2023_2024 = yf.download(list(tickers_list), start=2023-01-01, end=2024-01-01, auto_adjust=True)["Close"]
 datos2023_2024.head()
+
 
