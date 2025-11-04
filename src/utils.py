311c1d3e0b8c34837e0ece491ec2d1ee5b58f702
Romen <romequinco@gmail.com>
2025-11-04 16:30:43 +0100

    Delete src/utils.py

--- src/utils.py
+++
@@ -1,5 +0,0 @@
-#funciones para usar a menudo
-#class(api1)
-#class(api2)
-#class(representar los datos)
-#para añadir estas funciones en otro .py tengo que poner en ese codigo from utils import (nombre func/clas?)
