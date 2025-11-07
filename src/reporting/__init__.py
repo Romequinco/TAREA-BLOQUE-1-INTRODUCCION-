a72a5eb604ee0638c35a3adfeebd28253748aae6
Romen <romequinco@gmail.com>
2025-11-07 17:04:43 +0100

    Create __init__.py

---
+++ src/reporting/__init__.py
@@ -0,0 +1,8 @@
+"""Paquete de reporting: markdown y visualizaciones."""
+
+from .markdown_report import MarkdownReportGenerator
+from .visualizations import VisualizationReport
+
+__all__ = ["MarkdownReportGenerator", "VisualizationReport"]
+
+
