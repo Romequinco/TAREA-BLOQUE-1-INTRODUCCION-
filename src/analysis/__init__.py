fee2cbeb121d895fb428ca3aaf32500ca6c3b5e1
Romen <romequinco@gmail.com>
2025-11-07 17:08:41 +0100

    Create __init__.py

---
+++ src/analysis/__init__.py
@@ -0,0 +1,7 @@
+"""Módulos analíticos: simulaciones, riesgos y métricas avanzadas."""
+
+from .monte_carlo import MonteCarloResult, MonteCarloSimulator
+
+__all__ = ["MonteCarloSimulator", "MonteCarloResult"]
+
+
